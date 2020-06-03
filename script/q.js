d55425419cddcc47337fb1ddab287243e731fc26
Yury Kuppa <x-2@mail.ru>
2020-06-03 21:19:25 +0300

    something

--- script/q.js
+++ script/q.js
@@ -28,6 +28,126 @@ export const questions = [
     },
     {
         q10: 'Fugit hic vitae odio eos eveniet laudantium, voluptatibus ipsum facere?'
+    },
+    {
+        q11: 'Unde culpa quae animi perferendis, repellat tempora facilis fugiat eligendi'
+    },
+    {
+        q12: 'Ullam error numquam quos accusamus aperiam iure non quammpariatur earum. Porro architecto vel assumenda id. Obcaecati porro eius, dolorem magni suscipit nostrummsaepe dolores inventore'
+    },
+    {
+        q13: 'Fugit hic vitae odio eos eveniet laudantium, voluptatibus ipsum facere?'
+    },
+    {
+        q14: 'Unde culpa quae animi perferendis, repellat tempora facilis fugiat eligendi'
+    },
+    {
+        q15: 'Ullam error numquam quos accusamus aperiam iure non quammpariatur earum. Porro architecto vel assumenda id. Obcaecati porro eius, dolorem magni suscipit nostrummsaepe dolores inventore'
+    },
+    {
+        q16: 'Fugit hic vitae odio eos eveniet laudantium, voluptatibus ipsum facere?'
+    },
+    {
+        q17: 'Unde culpa quae animi perferendis, repellat tempora facilis fugiat eligendi'
+    },
+    {
+        q18: 'Ullam error numquam quos accusamus aperiam iure non quammpariatur earum. Porro architecto vel assumenda id. Obcaecati porro eius, dolorem magni suscipit nostrummsaepe dolores inventore'
+    },
+    {
+        q19: 'Fugit hic vitae odio eos eveniet laudantium, voluptatibus ipsum facere?'
+    },
+    {
+        q20: 'Unde culpa quae animi perferendis, repellat tempora facilis fugiat eligendi'
+    },
+    {
+        q21: 'Unde culpa quae animi perferendis, repellat tempora facilis fugiat eligendi'
+    },
+    {
+        q22: 'Ullam error numquam quos accusamus aperiam iure non quammpariatur earum. Porro architecto vel assumenda id. Obcaecati porro eius, dolorem magni suscipit nostrummsaepe dolores inventore'
+    },
+    {
+        q23: 'Fugit hic vitae odio eos eveniet laudantium, voluptatibus ipsum facere?'
+    },
+    {
+        q24: 'Unde culpa quae animi perferendis, repellat tempora facilis fugiat eligendi'
+    },
+    {
+        q25: 'Unde culpa quae animi perferendis, repellat tempora facilis fugiat eligendi'
+    },
+    {
+        q26: 'Ullam error numquam quos accusamus aperiam iure non quammpariatur earum. Porro architecto vel assumenda id. Obcaecati porro eius, dolorem magni suscipit nostrummsaepe dolores inventore'
+    },
+    {
+        q27: 'Fugit hic vitae odio eos eveniet laudantium, voluptatibus ipsum facere?'
+    },
+    {
+        q28: 'Unde culpa quae animi perferendis, repellat tempora facilis fugiat eligendi'
+    },
+    {
+        q29: 'Unde culpa quae animi perferendis, repellat tempora facilis fugiat eligendi'
+    },
+    {
+        q30: 'Ullam error numquam quos accusamus aperiam iure non quammpariatur earum. Porro architecto vel assumenda id. Obcaecati porro eius, dolorem magni suscipit nostrummsaepe dolores inventore'
+    },
+    {
+        q31: 'Fugit hic vitae odio eos eveniet laudantium, voluptatibus ipsum facere?'
+    },
+    {
+        q32: 'Unde culpa quae animi perferendis, repellat tempora facilis fugiat eligendi'
+    },
+    {
+        q33: 'Unde culpa quae animi perferendis, repellat tempora facilis fugiat eligendi'
+    },
+    {
+        q34: 'Ullam error numquam quos accusamus aperiam iure non quammpariatur earum. Porro architecto vel assumenda id. Obcaecati porro eius, dolorem magni suscipit nostrummsaepe dolores inventore'
+    },
+    {
+        q35: 'Fugit hic vitae odio eos eveniet laudantium, voluptatibus ipsum facere?'
+    },
+    {
+        q36: 'Unde culpa quae animi perferendis, repellat tempora facilis fugiat eligendi'
+    },
+    {
+        q37: 'Unde culpa quae animi perferendis, repellat tempora facilis fugiat eligendi'
+    },
+    {
+        q38: 'Ullam error numquam quos accusamus aperiam iure non quammpariatur earum. Porro architecto vel assumenda id. Obcaecati porro eius, dolorem magni suscipit nostrummsaepe dolores inventore'
+    },
+    {
+        q39: 'Fugit hic vitae odio eos eveniet laudantium, voluptatibus ipsum facere?'
+    },
+    {
+        q40: 'Unde culpa quae animi perferendis, repellat tempora facilis fugiat eligendi'
+    },
+    {
+        q41: 'Unde culpa quae animi perferendis, repellat tempora facilis fugiat eligendi'
+    },
+    {
+        q42: 'Ullam error numquam quos accusamus aperiam iure non quammpariatur earum. Porro architecto vel assumenda id. Obcaecati porro eius, dolorem magni suscipit nostrummsaepe dolores inventore'
+    },
+    {
+        q43: 'Fugit hic vitae odio eos eveniet laudantium, voluptatibus ipsum facere?'
+    },
+    {
+        q44: 'Unde culpa quae animi perferendis, repellat tempora facilis fugiat eligendi'
+    },
+    {
+        q45: 'Unde culpa quae animi perferendis, repellat tempora facilis fugiat eligendi'
+    },
+    {
+        q46: 'Ullam error numquam quos accusamus aperiam iure non quammpariatur earum. Porro architecto vel assumenda id. Obcaecati porro eius, dolorem magni suscipit nostrummsaepe dolores inventore'
+    },
+    {
+        q47: 'Fugit hic vitae odio eos eveniet laudantium, voluptatibus ipsum facere?'
+    },
+    {
+        q48: 'Unde culpa quae animi perferendis, repellat tempora facilis fugiat eligendi'
+    },
+    {
+        q49: 'Ullam error numquam quos accusamus aperiam iure non quammpariatur earum. Porro architecto vel assumenda id. Obcaecati porro eius, dolorem magni suscipit nostrummsaepe dolores inventore'
+    },
+    {
+        q50: 'Fugit hic vitae odio eos eveniet laudantium, voluptatibus ipsum facere?'
     }
 
 ]
--- script/testing.js
+++ script/testing.js
@@ -1,10 +1,40 @@
-import { questions as q } from './q.js';
+import { questions as q, questions } from './q.js';
 import { variants as v } from './v.js'
 import { answers as a } from './a.js'
 
 document.addEventListener('DOMContentLoaded', function () {
 
-    for 
+    let questionsDiv = document.querySelector('.questions');
+
+    questionsDiv.insertAdjacentHTML('afterbegin', `
+        <div class="question-block">
+        <p class="question">вопрос</p>
+        <form class="answers">
+            <div>
+                <label for="answer1">
+                    <input type="radio" id='answer1' name='question-1'> Вариант
+                            </label>
+                        </div>
+                <div>
+                    <label for="answer2">
+                        <input type="radio" id='answer2' name='question-1'>Вариант
+                            </label>
+                        </div>
+                    <div>
+                        <label for="answer3">
+                            <input type="radio" id='answer3' name='question-1'>Вариант
+                            </label>
+                        </div>
+                        <div>
+                            <label for="answer4">
+                                <input type="radio" id='answer4' name='question-1'>Вариант
+                            </label>
+                        </div>
+                    </form>
+                    </div>
+    `)
+
+
 
 
 })
