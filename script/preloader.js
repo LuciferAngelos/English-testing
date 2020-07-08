d1411c725eb97d19ad64831ed62f0bef3c0c1124
Yury Kuppa <x-2@mail.ru>
2020-07-08 16:01:27 +0300

    1

--- script/preloader.js
+++ script/preloader.js
@@ -1,4 +1,4 @@
-window.addEventListener('DOMContentLoaded', function () {
+window.addEventListener('load', function () {
     document.body.classList.add('loaded_hiding');
     window.setTimeout(function () {
         document.body.classList.add('loaded');
